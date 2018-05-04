cac36663337c9539f03519dda11093ed9f9803ac
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-05-04 11:55:51 +0200

    dfghjk

---
+++ js/script.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+    $.fn.clickToggle = function (func1, func2) {
+        var funcs = [func1, func2];
+        this.data('toggleclicked', 0);
+        this.click(function () {
+            var data = $(this).data();
+            var tc = data.toggleclicked;
+            $.proxy(funcs[tc], this)();
+            data.toggleclicked = (tc + 1) % 2;
+        });
+        return this;
+    };
+}(jQuery));
+/* Allt ska vara udner denna */
+$(document).ready(function () {
+
+/* hamburger animate*/
+    $('.hamburger').click(function () {
+        $('.hamburger').toggleClass('is-active');
+
+
+    });
+    
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 8000);    
+}
+
+});
+
+
